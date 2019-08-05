7c6066ca44619e8037b54db2dbc6c7c5aa65e995
jakinpilla <jakinpilla@gmail.com>
2019-08-05 20:27:56 +0900

    updated

---
+++ intro_to_Python/python_basic.py
@@ -0,0 +1,58 @@
+print(18 % 8)
+print(100*1.1**7)
+
+height = 1.79
+weight = 68.7
+
+bmi = weight / height**2
+bmi
+
+# sPython Type...
+type(bmi)
+
+'ab' + 'cd'
+# Different type = different behavior!
+
+savings = 100
+result = 100*1.10**7
+
+print("I started with $"+ str(savings) + "and now have $" + str(result) + ". Aewsome!")
+
+# List...[a, b, c]
+
+fam = ["liz", 1.73, "emma", 1.86, "mom", 1.71, "dad", 189]
+fam
+
+# sublist...
+fam2 = [["liz", 1.73], ["emma", 1.86], ["mom", 1.71], ["dad", 189]]
+
+
+type(fam)
+type(fam2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
