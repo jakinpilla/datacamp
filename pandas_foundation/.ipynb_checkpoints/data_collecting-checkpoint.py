1fe85a6634a6fa33f7315a8b3266d55a8615559a
jakinpilla <jakinpilla@gmail.com>
2019-08-09 19:23:36 +0900

    updated

---
+++ pandas_foundation/.ipynb_checkpoints/data_collecting-checkpoint.py
@@ -0,0 +1,20 @@
+from os import getcwd, chdir
+#!/usr/bin/env python
+!pwd
+chdir("C:\\Users\\Daniel\\datacamp\\pandas_foundation")
+getcwd()
+
+
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 100)
+
+import os
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = 'C:/Anaconda3/Library/plugins/platforms'
+getcwd()
+
+
+
+
