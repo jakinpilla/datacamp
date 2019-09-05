89682a3e222ae2c329ca4859cff00b63bb8ebc9c
jakinpilla <jakinpilla@gmail.com>
2019-09-06 07:02:08 +0900

    'updated'

--- Merging dataframes with pandas/ch01_001.py
+++ Merging dataframes with pandas/ch01_001.py
@@ -133,3 +133,72 @@ common_names = common_names.dropna()
 
 # Print shape of new common_names
 print(common_names.shape)
+
+##----
+
+weather = pd.read_csv('./data/pittsburgh2013.csv', index_col = 'Date', parse_dates=True)
+# Extract selected columns from weather as new DataFrame: temps_f
+temps_f = weather[['Min TemperatureF', 'Mean TemperatureF', 
+'Max TemperatureF']]
+
+# Convert temps_f to celsius: temps_c
+temps_c = (temps_f - 32) * 5/9
+
+# Rename 'F' in column names with 'C': temps_c.columns
+temps_c.columns = temps_c.columns.str.replace('F', 'C')
+
+# Print first 5 rows of temps_c
+print(temps_c.head())
+
+import pandas as pd
+
+## No data...----
+
+# Read 'GDP.csv' into a DataFrame: gdp
+gdp = pd.read_csv('./data/gdp_china.csv')
+
+gdp = pd.read_csv('./data/gdp_china.csv', index_col = 'DATE', 
+parse_dates=True)
+
+# Slice all the gdp data from 2008 onward: post2008
+post2008 = ____
+
+# Print the last 8 rows of post2008
+print(post2008.tail(8))
+
+# Resample post2008 by year, keeping last(): yearly
+yearly = ____
+
+# Print yearly
+print(yearly)
+
+# Compute percentage growth of yearly: yearly['growth']
+yearly['growth'] = ____
+
+# Print yearly again
+print(yearly)
+
+## ----
+
+# Import pandas
+import pandas as pd
+
+# Read 'sp500.csv' into a DataFrame: sp500
+sp500 = pd.read_csv('./data/sp500.csv', 
+index_col = 'Date', parse_dates = True)
+
+# Read 'exchange.csv' into a DataFrame: exchange
+exchange = pd.read_csv('./data/exchange.csv', 
+index_col = 'Date', parse_dates = True)
+
+# Subset 'Open' & 'Close' columns from sp500: dollars
+dollars = sp500[['Open', 'Close']]
+
+# Print the head of dollars
+print(dollars.head())
+
+# Convert dollars to pounds: pounds
+pounds = dollars.multiply(exchange['GBP/USD'], axis='rows')
+
+# Print the head of pounds
+print(pounds.head())
