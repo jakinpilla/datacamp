bab18ae061ce5250f83c7c082478ddc6d81b90e7
jakinpilla <jakinpilla@gmail.com>
2019-09-12 22:53:19 +0900

    'updated'

--- CorrelationAndRegression/ch01_001.R
+++ CorrelationAndRegression/ch01_001.R
@@ -1,9 +1,51 @@
 library(tidyverse)
-
 library(openintro)
+
 data('ncbirths')
 
 ncbirths %>% head()
 ncbirths %>% colnames()
 
 ggplot(ncbirths, aes(weeks, weight)) + geom_point()
+
+
+# Boxplot of weight vs. weeks
+ggplot(data = ncbirths, 
+       aes(x = cut(weeks, breaks = 5), y = weight)) + geom_boxplot()
+
+
+data('mammals')
+mammals %>% head()
+mammals %>% colnames()
+
+ggplot(mammals, aes(BodyWt, BrainWt)) + 
+  geom_point()
+
+data('mlbBat10')
+mlbBat10 %>% colnames()
+ggplot(mlbBat10, aes(OBP, SLG)) + geom_point()
+
+data('bdims')
+bdims %>% colnames()
+ggplot(bdims, aes(hgt, wgt, color = factor(sex))) + 
+  geom_point()
+
+data('smoking')
+smoking %>% colnames()
+ggplot(smoking, aes(age, amtWeekdays)) + geom_point()
+
+
+mammals %>% colnames()
+# Scatterplot with coord_trans()
+ggplot(data = mammals, aes(x = BodyWt, y = BrainWt)) +
+  geom_point() + 
+  coord_trans(x = "log10", y = "log10")
+
+# Scatterplot with scale_x_log10() and scale_y_log10()
+ggplot(data = mammals, aes(x = BodyWt, y = BrainWt)) +
+  geom_point() +
+  scale_x_log10() + 
+  scale_y_log10()
+
+
+
