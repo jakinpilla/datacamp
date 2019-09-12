1b216b761765914b293e2216361bcd792f344e6d
jakinpilla <jakinpilla@gmail.com>
2019-09-12 23:01:10 +0900

    'updated'

--- CorrelationAndRegression/ch01_001.R
+++ CorrelationAndRegression/ch01_001.R
@@ -48,4 +48,14 @@ ggplot(data = mammals, aes(x = BodyWt, y = BrainWt)) +
   scale_y_log10()
 
 
+# Filter for AB greater than or equal to 200
+ab_gt_200 <- mlbBat10 %>%
+  filter(AB >= 200) 
 
+# Scatterplot of SLG vs. OBP
+ggplot(ab_gt_200, aes(x = OBP, y = SLG)) +
+  geom_point()
+
+# Identify the outlying player
+ab_gt_200 %>%
+  filter(OBP < .2)
