5af450352f915f246a42174d2faedc0fe672d069
jakinpilla <jakinpilla@gmail.com>
2019-09-13 23:33:23 +0900

    'updated'

--- StatiscalThikingInPython_1/ch04_001.py
+++ StatiscalThikingInPython_1/ch04_001.py
@@ -140,3 +140,60 @@ plt.ylabel('PDF')
 plt.show()
 
 
+data = np.genfromtxt('./data/titanic_sub.csv', delimiter=',', names=True, dtype=None)
+
+data['Survived']
+
+# Assign the filename: file
+file = './data/titanic_sub.csv'
+
+# Import file using np.recfromcsv: d
+d = np.recfromcsv(file)
+
+# Print out first three entries of d
+print(d[:3])
+
+
+# Import pandas as pd
+import pandas as pd
+
+# Assign the filename: file
+file = './data/titanic_sub.csv'
+
+# Read the file into a DataFrame: df
+df = pd.read_csv(file)
+
+# View the head of the DataFrame
+df.head()
+
+
+# Assign the filename: file
+file = './data/mnist_kaggle_some_rows.csv'
+
+# Read the first 5 rows of the file into a DataFrame: data
+data = pd.read_csv(file, nrows = 5, header = None)
+
+# Build a numpy array from the DataFrame: data_array
+data_array = data.values
+
+# Print the datatype of data_array to the shell
+print(type(data_array))
+
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Assign filename: file
+file = 'titanic_corrupt.txt'
+
+# Import file: data
+data = pd.read_csv(file, sep='\t', comment='#', na_values='Nothing')
+
+# Print the head of the DataFrame
+print(data.head())
+
+# Plot 'Age' variable in a histogram
+pd.DataFrame.hist(data[['Age']])
+plt.xlabel('Age (years)')
+plt.ylabel('count')
+plt.show()
