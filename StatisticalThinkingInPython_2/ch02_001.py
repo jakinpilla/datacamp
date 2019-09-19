d1686e63c629e74b23469dd587626959265495eb
jakinpilla <jakinpilla@gmail.com>
2019-09-19 21:11:09 +0900

    updated

--- StatisticalThinkingInPython_2/ch02_001.py
+++ StatisticalThinkingInPython_2/ch02_001.py
@@ -221,7 +221,6 @@ _ = plt.ylabel('PDF')
 plt.show()
 
 
-
 # Generate array of x-values for bootstrap lines: x
 x = np.array([0, 100])
 
@@ -242,10 +241,129 @@ plt.margins(0.02)
 plt.show()
 
 
+votes = pd.read_csv('./data/2008_swing_states.csv')
+dem_share_PA = np.array(votes[votes.state == 'PA'].dem_share)
+dem_share_OH = np.array(votes[votes.state == 'OH'].dem_share)
+
+dem_share_both = np.concatenate((dem_share_PA, dem_share_OH))
+
+dem_share_perm = np.random.permutation(dem_share_both)
+
+perm_sample_PA = dem_share_perm[:len(dem_share_PA)]
 
 
+def permutation_sample(data1, data2):
+    """Generate a permutation sample from two data sets."""
+    # Concatenate the data sets: data
+    data = np.concatenate((data1, data2))
+    # Permute the concatenated array: permuted_data
+    permuted_data = np.random.permutation(data)
+    # Split the permuted array into two: perm_sample_1, perm_sample_2
+    perm_sample_1 = permuted_data[:len(data1)]
+    perm_sample_2 = permuted_data[len(data1):]
+    return perm_sample_1, perm_sample_2
 
 
+weather = pd.read_csv('./data/sheffield_weather_station.csv')
+
+rain_june = np.array(weather[weather.mm == 6].rain)
+rain_november = np.array(weather[weather.mm == 11].rain)
+
+plt.clf()
+
+for _ in range(50):
+    # Generate permutation samples
+    perm_sample_1, perm_sample_2 = permutation_sample(rain_june, rain_november)
+    # Compute ECDFs
+    x_1, y_1 = ecdf(perm_sample_1)
+    x_2, y_2 = ecdf(perm_sample_2)
+    # Plot ECDFs of permutation sample
+    _ = plt.plot(x_1, y_1, marker='.', linestyle='none',
+                 color='red', alpha=0.02)
+    _ = plt.plot(x_2, y_2, marker='.', linestyle='none',
+                 color='blue', alpha=0.02)
+
+plt.show()
+                 
+# Create and plot ECDFs from original data
+x_1, y_1 = ecdf(rain_june)
+x_2, y_2 = ecdf(rain_november)
+_ = plt.plot(x_1, y_1, marker='.', linestyle='none', color='red')
+_ = plt.plot(x_2, y_2, marker='.', linestyle='none', color='blue')
+
+# Label axes, set margin, and show plot
+plt.margins(0.02)
+_ = plt.xlabel('monthly rainfall (mm)')
+_ = plt.ylabel('ECDF')
+plt.show()
+
+# Test Statics and p-values
+
+perm_sample_PA, perm_sample_OH= permutation_sample(dem_share_PA, dem_share_OH)
+
+np.mean(perm_sample_PA) - np.mean(perm_sample_OH)
+
+np.mean(dem_share_PA) - np.mean(dem_share_OH)
+
+# p-value: The probability of obtaining a value of your test statistic that is
+# at least as extreme as what was observed, under the assumption the null hypothesis is true...
 
 
+def draw_perm_reps(data_1, data_2, func, size=1):
+    """Generate multiple permutation replicates."""
+    # Initialize array of replicates: perm_replicates
+    perm_replicates = np.empty(size)
+    for i in range(size):
+        # Generate permutation sample
+        perm_sample_1, perm_sample_2 = permutation_sample(data_1, data_2)
+        # Compute the test statistic
+        perm_replicates[i] = func(perm_sample_1, perm_sample_2)
+    return perm_replicates
+    
+    
 
+frog = pd.read_csv('./data/frog_tongue.csv')
+
+frog = frog[['ID', 'impact force (mN)']]
+
+id = ['A', 'B', 'C', 'D']
+
+frog['ID'] = np.array(id).repeat(20)
+frog
+
+frog['impact_force'] = frog['impact force (mN)'] / 1000
+df = frog[['ID', 'impact_force']]
+df['ID'] = df.ID.astype('category')
+df.info()
+
+plt.clf()
+
+# Make bee swarm plot
+_ = sns.swarmplot('ID', 'impact_force', data = df)
+
+# Label axes
+_ = plt.xlabel('frog')
+_ = plt.ylabel('impact force (N)')
+
+# Show the plot
+plt.show()
+
+
+def diff_of_means(data_1, data_2):
+    """Difference in means of two arrays."""
+    # The difference of means of data_1, data_2: diff
+    diff = np.mean(data_1) - np.mean(data_2)
+    return diff
+
+# Compute difference of mean impact force from experiment: empirical_diff_means
+empirical_diff_means = ____
+
+# Draw 10,000 permutation replicates: perm_replicates
+perm_replicates = draw_perm_reps(____, ____,
+                                 ____, size=10000)
+
+# Compute p-value: p
+p = np.sum(____ >= ____) / len(____)
+
+# Print the result
+print('p-value =', p)
