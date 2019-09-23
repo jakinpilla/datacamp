b864ae3d01c2cc180631c15172c56087e6753d49
jakinpilla <jakinpilla@gmail.com>
2019-09-23 23:34:59 +0900

    'updated'

--- StatisticalThinkingInPython_2/ch05_001.py
+++ StatisticalThinkingInPython_2/ch05_001.py
@@ -12,3 +12,424 @@ pd.set_option('display.width', 100)
 import matplotlib.pyplot as plt 
 import seaborn as sns
 
+##
+
+finch_1975  = pd.read_csv('./data/finch_beaks_1975.csv')
+finch_1975['year'] = '1975'
+
+finch_1975.columns
+
+finch_1975_1 = finch_1975[finch_1975['species'] == 'scandens']
+
+finch_1975_1.columns
+
+finch_1975_1
+
+finch_1975_1 = finch_1975_1[['Beak depth, mm', 'year']]
+
+finch_1975_1.rename(columns = {'Beak depth, mm': 'beak_depth'}, inplace = True)
+
+finch_1975_1.head()
+
+##-----
+
+finch_2012  = pd.read_csv('./data/finch_beaks_2012.csv')
+finch_2012['year'] = '2012'
+
+finch_2012.columns
+
+finch_2012_1 = finch_2012[finch_2012['species'] == 'scandens']
+
+finch_2012_1.columns
+
+finch_2012_1
+
+finch_2012_1 = finch_2012_1[['bdepth', 'year']]
+
+finch_2012_1.rename(columns = {'bdepth': 'beak_depth'}, inplace = True)
+
+finch_2012_1.head()
+
+
+df = pd.concat([finch_1975_1, finch_2012_1], axis = 'rows')
+df.shape
+df.head()
+
+plt.clf()
+# Create bee swarm plot
+_ = sns.swarmplot(x = 'year', y = 'beak_depth', data=df)
+
+# Label the axes
+_ = plt.xlabel('year')
+_ = plt.ylabel('beak depth (mm)')
+
+# Show the plot
+plt.show()
+
+bd_1975 = np.array(finch_1975_1.beak_depth)
+bd_2012 = finch_2012_1.beak_depth.values
+
+# Compute ECDF
+x_1975, y_1975 = ecdf(bd_1975)
+x_2012, y_2012 = ecdf(bd_2012)
+
+# plt.clf()
+# Plot the ECDFs
+_ = plt.plot(x_1975, y_1975, marker='.', linestyle='none')
+_ = plt.plot(x_2012, y_2012, marker='.', linestyle='none')
+
+# Set margins
+plt.margins(.02)
+
+# Add axis labels and legend
+_ = plt.xlabel('beak depth (mm)')
+_ = plt.ylabel('ECDF')
+_ = plt.legend(('1975', '2012'), loc='lower right')
+
+# Show the plot
+plt.show()
+
+
+def bootstrap_replicate_1d(data, func):
+  '''Generate bootstrap replicate of 1D data.'''
+  bs_sample = np.random.choice(data, len(data))
+  return func(bs_sample)
+
+##
+def draw_bs_reps(data, func, size=1):
+    """Draw bootstrap replicates."""
+    # Initialize array of replicates: bs_replicates
+    bs_replicates = np.empty(size)
+    # Generate replicates
+    for i in range(size):
+        bs_replicates[i] = bootstrap_replicate_1d(data, func)
+    return bs_replicates
+
+# Compute the difference of the sample means: mean_diff
+mean_diff = np.mean(bd_2012) - np.mean(bd_1975)
+
+# Get bootstrap replicates of means
+bs_replicates_1975 = draw_bs_reps(bd_1975, np.mean, size = 10000)
+bs_replicates_2012 = draw_bs_reps(bd_2012, np.mean, size = 10000)
+
+# Compute samples of difference of means: bs_diff_replicates
+bs_diff_replicates = bs_replicates_2012 - bs_replicates_1975
+
+# Compute 95% confidence interval: conf_int
+conf_int = np.percentile(bs_diff_replicates, [2.5, 97.5])
+
+# Print the results
+print('difference of means =', mean_diff, 'mm')
+print('95% confidence interval =', conf_int, 'mm')
+
+
+# Hypothesis test: Are beaks deeper in 2012?
+
+# Compute the difference of the sample means: mean_diff
+mean_diff = np.mean(bd_2012) - np.mean(bd_1975)
+
+# Compute mean of combined data set: combined_mean
+combined_mean = np.mean(np.concatenate((bd_1975, bd_2012)))
+
+# Shift the samples
+# hint: To shift bd_1975 and bd_2012 as described in the instructions, you should 
+# subtract their individual means from themselves and add the combined_mean you calculated above.
+
+bd_1975_shifted = bd_1975 - np.mean(bd_1975) + combined_mean
+bd_2012_shifted = bd_2012 - np.mean(bd_2012) + combined_mean
+
+# Get bootstrap replicates of shifted data sets
+# hint : Since you need the bootstrap replicates of the mean, the argument to the func 
+# parameter of draw_bs_reps() should be np.mean.
+bs_replicates_1975 = draw_bs_reps(bd_1975_shifted, np.mean, size = 10000)
+bs_replicates_2012 = draw_bs_reps(bd_2012_shifted, np.mean, size = 10000)
+
+# Compute replicates of difference of means: bs_diff_replicates
+bs_diff_replicates = bs_replicates_2012 - bs_replicates_1975
+
+# Compute the p-value
+p = np.sum(bs_diff_replicates >= mean_diff) / len(bs_diff_replicates)
+
+# Print p-value
+print('p =', p)
+
+## Data for beak length ---
+
+## ----
+
+finch_1975  = pd.read_csv('./data/finch_beaks_1975.csv')
+finch_1975['year'] = '1975'
+
+finch_1975.columns
+
+finch_1975_1 = finch_1975[finch_1975['species'] == 'scandens']
+
+finch_1975_1.columns
+
+finch_1975_1
+
+finch_1975_1 = finch_1975_1[['Beak length, mm', 'year']]
+
+finch_1975_1.rename(columns = {'Beak length, mm': 'beak_length'}, inplace = True)
+
+finch_1975_1.head()
+
+##-----
+
+finch_2012  = pd.read_csv('./data/finch_beaks_2012.csv')
+finch_2012['year'] = '2012'
+
+finch_2012.columns
+
+finch_2012_1 = finch_2012[finch_2012['species'] == 'scandens']
+
+finch_2012_1.columns
+
+finch_2012_1
+
+finch_2012_1 = finch_2012_1[['blength', 'year']]
+
+finch_2012_1.rename(columns = {'blength': 'beak_length'}, inplace = True)
+
+finch_2012_1.head()
+
+
+# df = pd.concat([finch_1975_1, finch_2012_1], axis = 'rows')
+# df.shape
+# df.head()
+
+bl_1975 = finch_1975_1.beak_length.values
+
+bl_2012 = finch_2012_1.beak_length.values
+
+
+bd_1975
+bd_2012
+bl_1975
+bl_2012
+
+plt.clf()
+# Make scatter plot of 1975 data
+_ = plt.plot(bl_1975, bd_1975, marker='.',
+             linestyle='None', color = 'blue', alpha = .5)
+
+# Make scatter plot of 2012 data
+_ = plt.plot(bl_2012, bd_2012, marker='.',
+            linestyle='None', color = 'red', alpha = .5)
+
+# Label axes and make legend
+_ = plt.xlabel('beak length (mm)')
+_ = plt.ylabel('beak depth (mm)')
+_ = plt.legend(('1975', '2012'), loc='upper left')
+
+# Show the plot
+plt.show()
+
+# Define draw_pairs_linreg() ----
+
+def draw_bs_pairs_linreg(x, y, size=1):
+    """Perform pairs bootstrap for linear regression."""
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+    # Initialize replicates: bs_slope_reps, bs_intercept_reps
+    bs_slope_reps = np.empty(size)
+    bs_intercept_reps = np.empty(size)
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds, size=len(inds)) # bootstrap...
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_slope_reps[i], bs_intercept_reps[i] = np.polyfit(bs_x, bs_y, 1)
+    return bs_slope_reps, bs_intercept_reps
+    
+
+# Compute the linear regressions
+slope_1975, intercept_1975 = np.polyfit(bl_1975, bd_1975, 1)
+slope_2012, intercept_2012 = np.polyfit(bl_2012, bd_2012, 1)
+
+# Perform pairs bootstrap for the linear regressions
+bs_slope_reps_1975, bs_intercept_reps_1975 = draw_bs_pairs_linreg(bl_1975, bd_1975, 1000)
+        
+bs_slope_reps_2012, bs_intercept_reps_2012 = draw_bs_pairs_linreg(bl_2012, bd_2012, 1000)
+
+# Compute confidence intervals of slopes
+slope_conf_int_1975 = np.percentile(bs_slope_reps_1975, [2.5, 97.5])
+slope_conf_int_2012 = np.percentile(bs_slope_reps_2012, [2.5, 97.5])
+
+intercept_conf_int_1975 = np.percentile(bs_intercept_reps_1975, [2.5, 97.5])
+intercept_conf_int_2012 = np.percentile(bs_intercept_reps_2012, [2.5, 97.5])
+
+# Print the results
+print('1975: slope =', slope_1975, 'conf int =', slope_conf_int_1975)
+print('1975: intercept =', intercept_1975,'conf int =', intercept_conf_int_1975)
+print('2012: slope =', slope_2012, 'conf int =', slope_conf_int_2012)
+print('2012: intercept =', intercept_2012, 'conf int =', intercept_conf_int_2012)
+
+
+plt.clf()
+# Make scatter plot of 1975 data
+_ = plt.plot(bl_1975, bd_1975, marker='.', linestyle='none', color='blue', alpha=0.5)
+
+# Make scatter plot of 2012 data
+_ = plt.plot(bl_2012, bd_2012, marker='.', linestyle='none', color='red', alpha=0.5)
+
+# Label axes and make legend
+_ = plt.xlabel('beak length (mm)')
+_ = plt.ylabel('beak depth (mm)')
+_ = plt.legend(('1975', '2012'), loc='upper left')
+
+# Generate x-values for bootstrap lines: x
+x = np.array([10, 17])
+
+# Plot the bootstrap lines
+for i in range(100):
+    plt.plot(x, bs_slope_reps_1975[i] * x + bs_intercept_reps_1975[i],
+             linewidth=0.5, alpha=0.2, color='blue')
+    plt.plot(x, bs_slope_reps_2012[i] * x  + bs_intercept_reps_2012[i],
+             linewidth=0.5, alpha=0.2, color='red')
+
+# Draw the plot again
+plt.show()
+
+
+# Compute length-to-depth ratios
+ratio_1975 = bl_1975/bd_1975
+ratio_2012 = bl_2012/bd_2012
+
+# Compute means
+mean_ratio_1975 = np.mean(ratio_1975)
+mean_ratio_2012 = np.mean(ratio_2012)
+
+# Generate bootstrap replicates of the means
+bs_replicates_1975 = draw_bs_reps(ratio_1975, np.mean, 10000)
+bs_replicates_2012 = draw_bs_reps(ratio_2012, np.mean, 10000)
+
+# Compute the 99% confidence intervals
+conf_int_1975 = np.percentile(bs_replicates_1975, [.5, 99.5])
+conf_int_2012 = np.percentile(bs_replicates_2012, [.5, 99.5])
+
+# Print the results
+print('1975: mean ratio =', mean_ratio_1975, 'conf int =', conf_int_1975)
+print('2012: mean ratio =', mean_ratio_2012, 'conf int =', conf_int_2012)
+
+
+## Heredity ----
+scandens = pd.read_csv('./data/scandens_beak_depth_heredity.csv')
+fortis = pd.read_csv('./data/fortis_beak_depth_heredity.csv')
+scandens.columns
+fortis.columns
+
+bd_parent_scandens = scandens['mid_parent'].values
+len(bd_parent_scandens)
+
+bd_offspring_scandens = scandens['mid_offspring'].values
+len(bd_offspring_scandens)
+
+fortis.head()
+fortis['mid_parent'] = (fortis['Male BD'] + fortis['Female BD']) /2
+
+bd_parent_fortis = fortis.mid_parent.values
+len(bd_parent_fortis)
+
+bd_offspring_fortis = fortis['Mid-offspr'].values
+len(bd_offspring_fortis)
+
+plt.clf()
+# Make scatter plots
+_ = plt.plot(bd_parent_fortis, bd_offspring_fortis, marker='.', linestyle='none', color='blue', alpha=.5)
+_ = plt.plot(bd_parent_scandens, bd_offspring_scandens, marker='.', linestyle='none', color='red', alpha=.5)
+
+# Label axes
+_ = plt.xlabel('parental beak depth (mm)')
+_ = plt.ylabel('offspring beak depth (mm)')
+
+# Add legend
+_ = plt.legend(('G. fortis', 'G. scandens'), loc='lower right')
+
+# Show plot
+plt.show()
+
+
+tmp = np.arange(3)
+np.random.choice(tmp, len(tmp))
+
+def draw_bs_pairs(x, y, func, size=1):
+    """Perform pairs bootstrap for a single statistic."""
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+    # Initialize replicates: bs_replicates
+    bs_replicates = np.empty(size)
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds, len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_replicates[i] = func(bs_x, bs_y)
+    return bs_replicates
+
+
+bd_parent_scandens
+bd_offspring_scandens
+
+bd_parent_fortis
+bd_offspring_fortis
+
+r_scandens = pearson_r(bd_parent_scandens, bd_offspring_scandens)
+r_fortis = pearson_r(bd_parent_fortis, bd_offspring_fortis)
+
+# Acquire 1000 bootstrap replicates of Pearson r
+bs_replicates_scandens = draw_bs_pairs(bd_parent_scandens, bd_offspring_scandens, pearson_r, 1000)
+
+bs_replicates_fortis = draw_bs_pairs(bd_parent_fortis, bd_offspring_fortis, pearson_r, 1000)
+
+
+# Compute 95% confidence intervals
+conf_int_scandens = np.percentile(bs_replicates_scandens, [2.5, 97.5])
+conf_int_fortis = np.percentile(bs_replicates_fortis, [2.5, 97.5])
+
+# Print results
+print('G. scandens:', r_scandens, conf_int_scandens)
+print('G. fortis:', r_fortis, conf_int_fortis)
+
+
+
+def heritability(parents, offspring):
+    """Compute the heritability from parent and offspring samples."""
+    covariance_matrix = np.cov(parents, offspring)
+    return covariance_matrix[0, 1] / covariance_matrix[0, 0]
+
+# Compute the heritability
+heritability_scandens = heritability(bd_parent_scandens, bd_offspring_scandens)
+heritability_fortis = heritability(bd_parent_fortis, bd_offspring_fortis)
+
+# Acquire 1000 bootstrap replicates of heritability
+replicates_scandens = draw_bs_pairs(
+        bd_parent_scandens, bd_offspring_scandens, heritability, size=1000)
+        
+replicates_fortis = draw_bs_pairs(
+        bd_parent_fortis, bd_offspring_fortis, heritability, size=1000)
+
+
+# Compute 95% confidence intervals
+conf_int_scandens = np.percentile(replicates_scandens, [2.5, 97.5])
+conf_int_fortis = np.percentile(replicates_fortis, [2.5, 97.5])
+
+# Print results
+print('G. scandens:', heritability_scandens, conf_int_scandens)
+print('G. fortis:', heritability_fortis, conf_int_fortis)
+
+
+
+# Initialize array of replicates: perm_replicates
+perm_replicates = np.empty(10000)
+
+# Draw replicates
+for i in range(10000):
+    # Permute parent beak depths
+    bd_parent_permuted = np.random.permutation(bd_parent_scandens)
+    perm_replicates[i] = heritability(bd_parent_permuted, bd_offspring_scandens)
+
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= heritability_scandens) / len(perm_replicates)
+
+# Print the p-value
+print('p-val =', p)
