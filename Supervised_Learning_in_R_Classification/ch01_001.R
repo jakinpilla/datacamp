1d5b41352fdc6b725619a745d6ecbef11ac9af59
jakinpilla <jakinpilla@gmail.com>
2019-10-12 14:03:26 +0900

    updated

---
+++ Supervised_Learning_in_R_Classification/ch01_001.R
@@ -0,0 +1,116 @@
+# kNN
+
+# Classification tasks for driverless cars...
+
+signs_total <- read_csv('./data/knn_traffic_signs.csv')
+signs_total %>% dim()
+
+colnames(signs_total)
+dim(signs_total)
+head(signs_total$sign_type)
+
+signs_total %>% distinct(sign_type)
+
+# library(caret)
+# 
+# not_test_idx <- createDataPartition(signs_total$sign_type, p=.8, list = F)[, 1] 
+# 
+# not_test_signs_total <- signs_total[not_test_idx, ]
+# test_signs_total <- signs_total[-not_test_idx, ]
+# 
+# 
+# train_idx <- createDataPartition(not_test_signs_total$sign_type, p=.9, list = F)[, 1] 
+# 
+# train_idx %>% length()
+# 
+# train_signs_total <- not_test_signs_total[train_idx, ]
+# valid_signs_total <- not_test_signs_total[-train_idx, ]
+# 
+# dim(train_signs_total)[1] + dim(valid_signs_total)[1] + dim(test_signs_total)[1]
+# 
+# nrow(signs_total)
+# 
+# test_dataset <- test_signs_total %>%
+#   select(-sign_type)
+
+signs_total %>%
+  filter(sample == 'train') %>%
+  select(-sample, -id) -> signs
+
+signs_total %>%
+  filter(sample == 'test') %>%
+  select(-sample, -id, -sign_type) -> next_sign
+
+# Load the 'class' package
+library(class)
+
+# Create a vector of labels
+sign_types <- signs$sign_type
+
+# Classify the next sign observed
+knn(train = signs[-1], test = next_sign, cl = sign_types)
+
+
+# Examine the structure of the signs dataset
+str(signs)
+
+# Count the number of signs of each type
+table(signs$sign_type)
+
+# Check r10's average red level by sign type
+aggregate(r10 ~ sign_type, data = signs, mean)
+
+
+
+# -------------------------------------------------------------------------
+
+signs_total %>%
+  filter(sample == 'train') %>%
+  select(-sample, -id) -> signs
+
+signs_total %>%
+  filter(sample == 'test') %>%
+  select(-sample, -id) -> test_signs
+
+# Use kNN to identify the test road signs
+sign_types <- signs$sign_type
+signs_pred <- knn(train = signs[-1], test = test_signs[-1], cl = sign_types)
+
+# Create a confusion matrix of the predicted versus actual values
+signs_actual <- test_signs$sign_type
+table(signs_pred, signs_actual)
+
+# Compute the accuracy
+mean(signs_pred == signs_actual)
+
+# Bigger k is not always better...
+
+signs_test <- test_signs
+
+# Compute the accuracy of the baseline model (default k = 1)
+k_1 <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types)
+mean(k_1 == signs_test$sign_type)
+
+# Modify the above to set k = 7
+k_7 <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types, k=7)
+mean(k_7 == signs_test$sign_type)
+
+# Set k = 15 and compare to the above
+k_15 <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types, k=15)
+mean(k_15 == signs_test$sign_type)
+
+
+# Use the prob parameter to get the proportion of votes for the winning class
+sign_pred <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types, k=7, prob = TRUE)
+
+# Get the "prob" attribute from the predicted classes
+sign_prob <- attr(sign_pred, "prob")
+
+# Examine the first several predictions
+head(sign_pred)
+
+# Examine the proportion of votes for the winning class
+head(sign_prob)
+
+
+
---
+++ Working_with_Dates_and_Times_in_R/ch04_001.R
@@ -0,0 +1,198 @@
+Sys.timezone()
+
+# IANA Timezones...
+OlsonNames()
+
+length(OlsonNames())
+
+# Manipulating timezones...
+mar_11 <- ymd_hms("2017-03-11 12:00:00", 
+                  tz = "America/Los_Angeles")
+
+mar_11
+
+# force_tz() :: change the timezone without changing the clock time...
+force_tz(mar_11, tzone = 'America/New_York')
+
+# with_tz() :: view the same intant in a different timezone...
+with_tz(mar_11, tzone = 'America/New_York')
+
+
+
+# -------------------------------------------------------------------------
+
+# Game2: CAN vs NZL in Edmonton
+game2 <- mdy_hm("June 11 2015 19:00")
+
+# Game3: CHN vs NZL in Winnipeg
+game3 <- mdy_hm("June 15 2015 18:30")
+
+# Set the timezone to "America/Edmonton"
+game2_local <- force_tz(game2, tzone = 'America/Edmonton')
+game2_local
+
+# Set the timezone to "America/Winnipeg"
+game3_local <- force_tz(game3, tzone = 'America/Winnipeg')
+game3_local
+
+# How long does the team have to rest?
+as.period(game2_local %--% game3_local)
+
+#  Unlike force_tz(), with_tz() isn't changing the underlying moment of time, just how it is displayed.
+
+now <- now()
+with_tz(now, "America/Los_Angeles")
+with_tz(now, "America/New_York")
+with_tz(now, "Pacific/Auckland")
+
+with_tz(now, "America/Los_Angeles") - with_tz(now,  "Pacific/Auckland")
+
+# What time is game2_local in NZ?
+with_tz(game2_local, tzone = 'Pacific/Auckland')
+
+# What time is game2_local in Corvallis, Oregon?
+with_tz(game2_local, tzone = 'America/Los_Angeles')
+
+# What time is game3_local in NZ?
+with_tz(game3_local, tzone = 'Pacific/Auckland')
+
+
+# -------------------------------------------------------------------------
+akl_hourly <- read_csv('./data/akl_weather_hourly_2016.csv')
+
+akl_hourly  <- akl_hourly_raw  %>% 
+  mutate(date = make_date(year = year, month = month, day = mday),
+    datetime_string = paste(date, time, sep = "T"),
+    datetime = ymd_hms(datetime_string)
+  )
+
+
+# Examine datetime and date_utc columns
+head(akl_hourly$datetime)
+head(akl_hourly$date_utc)
+
+# Force datetime to Pacific/Auckland
+akl_hourly <- akl_hourly %>%
+  mutate(
+    datetime = force_tz(datetime, tzone = 'Pacific/Auckland'))
+
+# Reexamine datetime
+head(akl_hourly$datetime)
+
+# Are datetime and date_utc the same moments
+table(akl_hourly$datetime - akl_hourly$date_utc)
+
+
+# 4 rows they are different. Can you guess which? Yup, the times where DST kicks in.
+
+
+# hms ---------------------------------------------------------------------
+
+
+# Import auckland hourly data 
+akl_hourly <- read_csv('./data/akl_weather_hourly_2016.csv')
+
+# Examine structure of time column
+str(akl_hourly$time)
+
+# Examine head of time column
+head(akl_hourly$time)
+
+# A plot using just time
+ggplot(akl_hourly, aes(x = time, y = temperature)) +
+  geom_line(aes(group = make_date(year, month, mday)), alpha = 0.2)
+
+
+# -------------------------------------------------------------------------
+
+# parse_date_time() can be slow....
+
+# install.packages('fasttime')
+library(fasttime)
+fastPOSIXct('2003-02-27')
+
+x <- '2001-02-27'
+parse_date_time(x, order = 'ymd')
+
+fast_strptime(x, format = '%Y-%m-%d')
+fast_strptime(x, format = '%y-%m-%d')
+
+# Formatting datetimes....
+my_stamp <- stamp('Tuesday October 10 2017')
+
+my_stamp(ymd('2003-02-27'))
+##??
+
+# install.packages('microbenchmark')
+library(microbenchmark)
+library(fasttime)
+
+# dates <- akl_hourly$date %>% as.character()
+
+# Examine structure of dates
+str(dates)
+
+# Use fastPOSIXct() to parse dates
+fastPOSIXct(dates) %>% str()
+
+# Compare speed of fastPOSIXct() to ymd_hms()
+microbenchmark(
+  ymd_hms = ymd_hms(dates),
+  fasttime = fastPOSIXct(dates),
+  times = 20)
+
+
+# Head of dates
+head(dates)
+
+# Parse dates with fast_strptime
+fast_strptime(dates, 
+              format = '%Y-%m-%d') %>% str()
+
+# Comparse speed to ymd_hms() and fasttime
+microbenchmark(
+  ymd_hms = ymd_hms(dates),
+  fasttime = fastPOSIXct(dates),
+  fast_strptime = fast_strptime(dates, 
+                      format = '%Y-%m-%d'),
+  times = 20)
+
+
+# stamp() -----------------------------------------------------------------
+
+# stamp() takes a string which should be an example of how the date should be formatted, and returns a function that can be used to format dates.
+
+
+# Create a stamp based on "Sep 20 2017"
+date_stamp <- stamp('Sep 20 2017')
+
+# Print date_stamp
+date_stamp
+
+# Call date_stamp on today()
+date_stamp(today())
+
+# Create and call a stamp based on "09/20/2017"
+stamp("09/20/2017")(today())
+
+finished <- "I finished 'Dates and Times' in R on Thursday, September 20, 2017!" 
+
+# Use string finished for stamp()
+stamp(finished)(today())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
